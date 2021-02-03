552b1055cf20d1fedeaf0356cd86219dabaac47d
chalosalvador <chalosalvador@gmail.com>
2021-02-02 19:09:07 -0500

    Primer commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+
+// https://stark-spire-22280.herokuapp.com/api/books
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [books, setBooks] = useState([]);
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      const response = await fetch(
+        "https://stark-spire-22280.herokuapp.com/api/books"
+      );
+      const json = await response.json();
+      console.log("json", json);
+      setBooks(json.data);
+      return json;
+    };
+
+    fetchBooks();
+  }, []);
+
+  const handleChangeCounter = (value) => {
+    const newCounter = counter + value;
+    if (newCounter >= 0 && newCounter <= 10) {
+      setCounter(newCounter);
+    } else {
+      alert("Valor no permitido");
+    }
+  };
+
+  // const obj = {
+  //   name: "Chalo",
+  //   lastname: "Salvador",
+  // };
+  // for (let property in obj) {
+  //   console.log("property", obj[property]);
+  // }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <input type="text" value={counter} onChange={() => {}} />
+      <button onClick={() => handleChangeCounter(1)}>Incrementar</button>
+      <button onClick={() => handleChangeCounter(-1)}>Disminuir</button>
+
+      <h1>Lista de libros</h1>
+      <ul>
+        {books.map((book) => (
+          <li key={book.id}>{book.title}</li>
+        ))}
+      </ul>
+    </>
   );
 }
 
